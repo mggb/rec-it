f9d489180ec97e80e6771379b6a48dc5eb48c358
Maxime Oger <maximeoger@MacBook-Pro-de-Maxime-3.local>
2017-12-19 15:28:03 +0100

    avancement de l'inté

---
+++ js/player.js
@@ -0,0 +1,9 @@
+/* prendre les éléments */
+  const player = document.querySelector('.player');
+  const video = document.querySelector('.viewer');
+  const progress = document.querySelector('.progress');
+  const progressBar = document.querySelector('.progress__filled');
+  const toggle = document.querySelector('.toggle');
+/* Construire les fonctions */
+
+/* Accrocher les evetns listener */
